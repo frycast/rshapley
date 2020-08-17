71175a6b672bf6fb1438f9b294eb59917d6b5a65
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2020-08-17 13:11:35 +1000

    remove packrat

--- R/package_dev.R
+++ R/package_dev.R
@@ -1,8 +1,14 @@
+## This file contains code and comments related to package development.
+## It will be deleted at a later stage in development.
+
 # Some useful keyboard shortcuts for package authoring:
 #
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
+
+# Set up roxygen
+devtools::document()
 
 #' Hello World
 #'
@@ -12,3 +18,7 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+
+
+
