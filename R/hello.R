21a408c3f5b07fa0b8458eb98c968a213dabcb8b
Daniel Vidali Fryer <20966023+frycast@users.noreply.github.com>
2020-08-17 11:32:42 +1000

    package skeleton

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' Hello World
+#'
+#' A function for printing Hello World
+#'
+#' @value NULL
+hello <- function() {
+  print("Hello, world!")
+}
